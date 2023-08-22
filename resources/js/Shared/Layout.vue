3ab8d58d26109a47a3415b7a8f3f29e809748b85
fmeccanici <florismeccanici@mecctech-solutions.nl>
2023-08-22 21:07:45 +0200

    Episode 11 finished

--- resources/js/Shared/Layout.vue
+++ resources/js/Shared/Layout.vue
@@ -1,14 +1,15 @@
 <script setup>
-import {defineProps} from 'vue'
 import Nav from '../Shared/Nav.vue'
 
-defineProps({})
 </script>
 
 <template>
     <section class="p-6 bg-gray-200">
         <header class="flex justify-between">
-            <h1 class="font-bold text-lg">My App</h1>
+            <div class="flex items-center">
+                <h1 class="font-bold text-lg">My App</h1>
+                <p class="text-sm ml-4">Welcome Back, {{ $page.props.auth.user.username }}!</p>
+            </div>
             <Nav/>
         </header>
     </section>
--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,7 @@ use Inertia\Inertia;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Home', [
-        'name' => 'John Doe',
-        'frameworks' => [
-            'Laravel', 'Vue', 'Inertia'
-        ]
-    ]);
+    return Inertia::render('Home');
 });
 
 Route::get('/users', function () {
